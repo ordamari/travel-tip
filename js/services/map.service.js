1511a0da7a803c30ebb618ae95330525d12d4303
ordamari <ordamari971@gmail.com>
2020-08-19 13:44:41 +0300

    starter

---
+++ js/services/map.service.js
@@ -0,0 +1,57 @@
+
+export const mapService = {
+    initMap,
+    addMarker,
+    panTo
+}
+
+
+
+var map;
+
+export function initMap(lat = 29.55805, lng = 34.94821) {
+    console.log('InitMap');
+    return _connectGoogleApi()
+        .then(() => {
+            console.log('google available');
+            map = new google.maps.Map(
+                document.querySelector('#map'), {
+                center: { lat, lng },
+                zoom: 15
+            })
+            console.log('Map!', map);
+        })
+}
+
+function addMarker(loc) {
+    var marker = new google.maps.Marker({
+        position: loc,
+        map: map,
+        title: 'Hello World!'
+    });
+    return marker;
+}
+
+function panTo(lat, lng) {
+    var laLatLng = new google.maps.LatLng(lat, lng);
+    console.log("panTo -> laLatLng", laLatLng)
+    
+    map.panTo(laLatLng);
+}
+
+function _connectGoogleApi() {
+    if (window.google) return Promise.resolve()
+    const API_KEY = 'AIzaSyB0GFpf1xOP_iLzHNeqn5GFUugk38cbc6Y'; //TODO: Enter your API Key
+    var elGoogleApi = document.createElement('script');
+    elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
+    elGoogleApi.async = true;
+    document.body.append(elGoogleApi);
+
+    return new Promise((resolve, reject) => {
+        elGoogleApi.onload = resolve;
+        elGoogleApi.onerror = () => reject('Google script failed to load')
+    })
+}
+
+
+
